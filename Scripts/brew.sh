d58b5311ad2625fb5816e3b8d3f9fef80651d8ba
Marcin Iwanicki <miwanicki1@bloomberg.net>
2019-10-14 15:59:56 +0100

    Fix versions of swiftlint and swiftformat
    Signed-off-by: Marcin Iwanicki <miwanicki1@bloomberg.net>

--- Scripts/brew.sh
+++ Scripts/brew.sh
@@ -1,4 +1,16 @@
 #!/bin/zsh
 
-brew install swiftlint
-brew install swiftformat
+# don't pipe on failure
+set -o pipefail
+
+# exit on error
+trap 'exit' ERR
+
+# disable homebrew auto update
+export HOMEBREW_NO_AUTO_UPDATE=1
+
+# 0.35.0
+brew outdated swiftlint || brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/c6e596c7d301099a6f1d81c5e9a74a27dea0ade4/Formula/swiftlint.rb
+
+# 0.40.13
+brew outdated swiftformat || brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/0e69da465c5f1187521e9fa156639cd48d405f50/Formula/swiftformat.rb
