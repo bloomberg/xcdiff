d2b0e96233e6fc345b99337870b60d8e6a665298
akhazi@bloomberg.net <akhazi@bloomberg.net>
2019-10-16 09:50:57 +0100

    use json for manual and generated test commands
    Signed-off-by: akhazi@bloomberg.net <akhazi@bloomberg.net>

--- Scripts/generate_tests_commands_files.py
+++ Scripts/generate_tests_commands_files.py
@@ -5,6 +5,13 @@ import json
 import subprocess
 import sys
 import os
+import pathlib
+
+with open(pathlib.Path(__file__).parent / "../CommandTests/manual_test_commands.json", "r") as manual_test_commands_file:
+    manual_test_commands = json.load(manual_test_commands_file)
+
+with open(pathlib.Path(__file__).parent /  "../CommandTests/generated_test_commands.json", "r") as generated_test_commands_file:
+    generated_test_commands = json.load(generated_test_commands_file)
 
 #
 # Manually Specified Commands
@@ -15,55 +22,18 @@ import os
 #
 # - note: aliases need to be unique, otherwise the generated tests may overwrite one another!
 #
-COMMANDS = [
-    # list
-    ("list", ["-l"]),
+COMMANDS = [(manual_command["alias"], manual_command["command"]) for manual_command in manual_test_commands]
 
-    # unknown option
-    ("unknown", ["-unknown"]),
-
-    # non existing tags
-    ("non_existing_tag", ["-p1", "{ios_project_1}", "-p2", "{ios_project_2}", "-g", "targets, unsupported, unsupported_too"]),
-
-    # non existing target
-    ("non_existing_target", ["-p1", "{ios_project_1}", "-p2", "{ios_project_2}", "-t", "NON_EXISTING"]),
-
-    # differences only
-    ("differences_only", ["-p1", "{ios_project_1}", "-p2", "{ios_project_2}", "-d"]),
-
-    # non existing projects
-    ("p1_does_not_exist", ["-p1", "/not/existing/project1.xcodeproj", "-p2", "{ios_project_2}"]),
-    ("p2_does_not_exist", ["-p1", "{ios_project_1}", "-p2", "/not/existing/project2.xcodeproj"]),
-    ("p1_p2_do_not_exist", ["-p1", "/not/existing/project1.xcodeproj", "-p2", "/not/existing/project2.xcodeproj"]),
-
-    # target only in second project (targets tag)
-    ("target_only_in_second_g_targets", ["-p1", "{ios_project_1}", "-p2", "{ios_project_2}", "-g", "targets", "-t", "NewFramework", "-v"]),
-
-    # target only in second project (headers tag)
-    ("target_only_in_second_g_headers", ["-p1", "{ios_project_1}", "-p2", "{ios_project_2}", "-g", "headers", "-t", "NewFramework", "-v"]),
-]
-
-# 
+#
 # Auto generated command parameters
 #
 # The following parameters are used to auto generate command combinations
 # to use as tests.
 #
-TAGS = [
-    "",
-    "file_references",
-    "targets",
-    "headers",
-    "resources",
-    "configurations",
-    "settings",
-    "sources",
-    "source_trees",
-    "dependencies"
-]
-TARGETS = ["", "Project", "NewFramework"]
-FORMATS = ["", "console", "json", "markdown"]
-VERBOSE = ["", "-v"]
+TAGS = generated_test_commands["tags"]
+TARGETS = generated_test_commands["targets"]
+FORMATS = generated_test_commands["formats"]
+VERBOSE = generated_test_commands["verbose"]
 
 def generate_command(tag, target, format, verbose):
     command = [
@@ -113,40 +83,43 @@ def run_command(arguments):
     arguments = [project_1_path if element == "{ios_project_1}" else element for element in arguments]
     arguments = [project_2_path if element == "{ios_project_2}" else element for element in arguments]
     print(arguments)
-    return subprocess.Popen(arguments, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return subprocess.Popen(arguments, stdout=subprocess.PIPE, stderr=subprocess.PIPE,  shell=False)
 
 def generate_command_tests_files():
     dirname = os.path.dirname(sys.argv[0])
     command_tests_path = os.path.join(dirname, "../CommandTests/Generated")
     commands = generate_commands() + COMMANDS
     command_run = ["swift", "run", "xcdiff"]
-
     remove_all_command_tests_files(command_tests_path)
-    for command_tuple in commands:
-        alias = command_tuple[0]
-        command = command_tuple[1]
-        out = run_command(command_run + command)
-        output, _ = out.communicate()
-        output_string = output.decode(sys.stdout.encoding)
-        exit_code_string = str(out.returncode)
-        hash_string = hashlib.sha256(alias.encode("utf-8")).hexdigest()[:8]
-        filename = alias + "." + exit_code_string + "." + hash_string + ".md"
-        file_path = os.path.join(command_tests_path, filename)
-        f = open(file_path, "w")
-        f.write("# Command\n")
-        f.write("```json\n")
-        f.write(json.dumps(command))
-        f.write("\n")
-        f.write("```\n")
-        f.write("\n")
-        f.write("# Expected exit code\n")
-        f.write(exit_code_string)
-        f.write("\n\n")
-        f.write("# Expected output\n")
-        f.write("```\n")
-        f.write(output_string)
-        f.write("\n```\n")
-        f.close()
+
+    for command in commands:
+        write_test_file(command_run, command, command_tests_path)
+
+def write_test_file(command_run, command_tuple, command_tests_path):
+    alias = command_tuple[0]
+    command = command_tuple[1]
+    out = run_command(command_run + command)
+    output, _ = out.communicate()
+    output_string = output.decode(sys.stdout.encoding)
+    exit_code_string = str(out.returncode)
+    hash_string = hashlib.sha256(alias.encode("utf-8")).hexdigest()[:8]
+    filename = alias + "." + exit_code_string + "." + hash_string + ".md"
+    file_path = os.path.join(command_tests_path, filename)
+    f = open(file_path, "w")
+    f.write("# Command\n")
+    f.write("```json\n")
+    f.write(json.dumps(command))
+    f.write("\n")
+    f.write("```\n")
+    f.write("\n")
+    f.write("# Expected exit code\n")
+    f.write(exit_code_string)
+    f.write("\n\n")
+    f.write("# Expected output\n")
+    f.write("```\n")
+    f.write(output_string)
+    f.write("\n```\n")
+    f.close()
 
 def remove_all_command_tests_files(command_tests_directory):
     for file in os.listdir(command_tests_directory):
