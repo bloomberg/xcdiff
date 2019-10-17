fbf347c15a86d40d6009e16396aafa26caceb62e
Kas <kwridan@users.noreply.github.com>
2019-10-17 13:27:11 +0100

    Test improvements (#9)
    **Describe your changes**
    
    - Added a symlink to the two fixture projects to allow running `xcdiff` directly within the fixtures folder
    
    ```sh
    cd Fixtures
    xcdiff
    ```
    
    - Updated the test generator to allow specifying a manual set of commands (that previously were under `CommandTests/Manual`)
      - This eliminates the need to manually craft test markdown files, even for the cases where a manual command is needed
    
    **Testing performed**
    
    - Verified running `Scripts/generate_tests_commands_files.py` generates all tests including the manually specified ones
    - Verified running `xcdiff` within `Fixtures` works
    
    
    Signed-off-by: akhazi@bloomberg.net <akhazi@bloomberg.net>
    Signed-off-by: Kassem Wridan <kwridan@bloomberg.net>

---
+++ CommandTests/Generated/p1_p2_do_not_exist.1.98fc0060.md
@@ -0,0 +1,13 @@
+# Command
+```json
+["-p1", "/not/existing/project1.xcodeproj", "-p2", "/not/existing/project2.xcodeproj"]
+```
+
+# Expected exit code
+1
+
+# Expected output
+```
+ERROR: -p1 "/not/existing/project1.xcodeproj" project does not exist
+
+```
--- Scripts/generate_tests_commands_files.py
+++ Scripts/generate_tests_commands_files.py
@@ -5,20 +5,35 @@ import json
 import subprocess
 import sys
 import os
+import pathlib
 
-TAGS = ["",
-        "file_references",
-        "targets",
-        "headers",
-        "resources",
-        "configurations",
-        "settings",
-        "sources",
-        "source_trees",
-        "dependencies"]
-TARGETS = ["", "Project", "NewFramework"]
-FORMATS = ["", "console", "json", "markdown"]
-VERBOSE = ["", "-v"]
+with open(pathlib.Path(__file__).parent / "../CommandTests/manual_test_commands.json", "r") as manual_test_commands_file:
+    manual_test_commands = json.load(manual_test_commands_file)
+
+with open(pathlib.Path(__file__).parent /  "../CommandTests/generated_test_commands.json", "r") as generated_test_commands_file:
+    generated_test_commands = json.load(generated_test_commands_file)
+
+#
+# Manually Specified Commands
+#
+# The following commands will be used as tests.
+#
+# format: tuple `(alias_string, [command])`
+#
+# - note: aliases need to be unique, otherwise the generated tests may overwrite one another!
+#
+COMMANDS = [(manual_command["alias"], manual_command["command"]) for manual_command in manual_test_commands]
+
+#
+# Auto generated command parameters
+#
+# The following parameters are used to auto generate command combinations
+# to use as tests.
+#
+TAGS = generated_test_commands["tags"]
+TARGETS = generated_test_commands["targets"]
+FORMATS = generated_test_commands["formats"]
+VERBOSE = generated_test_commands["verbose"]
 
 def generate_command(tag, target, format, verbose):
     command = [
@@ -43,7 +58,7 @@ def generate_command(tag, target, format, verbose):
         command += ["-v"]
         alias += "_verbose"
 
-    return (command, alias)
+    return (alias, command)
 
 def generate_commands():
     commands = []
@@ -68,40 +83,43 @@ def run_command(arguments):
     arguments = [project_1_path if element == "{ios_project_1}" else element for element in arguments]
     arguments = [project_2_path if element == "{ios_project_2}" else element for element in arguments]
     print(arguments)
-    return subprocess.Popen(arguments, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return subprocess.Popen(arguments, stdout=subprocess.PIPE, stderr=subprocess.PIPE,  shell=False)
 
 def generate_command_tests_files():
     dirname = os.path.dirname(sys.argv[0])
     command_tests_path = os.path.join(dirname, "../CommandTests/Generated")
-    commands = generate_commands()
+    commands = generate_commands() + COMMANDS
     command_run = ["swift", "run", "xcdiff"]
-
     remove_all_command_tests_files(command_tests_path)
-    for command_tuple in commands:
-        command = command_tuple[0]
-        alias = command_tuple[1]
-        out = run_command(command_run + command)
-        output, _ = out.communicate()
-        output_string = output.decode(sys.stdout.encoding)
-        exit_code_string = str(out.returncode)
-        hash_string = hashlib.sha256(alias.encode("utf-8")).hexdigest()[:8]
-        filename = alias + "." + exit_code_string + "." + hash_string + ".md"
-        file_path = os.path.join(command_tests_path, filename)
-        f = open(file_path, "w")
-        f.write("# Command\n")
-        f.write("```json\n")
-        f.write(json.dumps(command))
-        f.write("\n")
-        f.write("```\n")
-        f.write("\n")
-        f.write("# Expected exit code\n")
-        f.write(exit_code_string)
-        f.write("\n\n")
-        f.write("# Expected output\n")
-        f.write("```\n")
-        f.write(output_string)
-        f.write("\n```\n")
-        f.close()
+
+    for command in commands:
+        write_test_file(command_run, command, command_tests_path)
+
+def write_test_file(command_run, command_tuple, command_tests_path):
+    alias = command_tuple[0]
+    command = command_tuple[1]
+    out = run_command(command_run + command)
+    output, _ = out.communicate()
+    output_string = output.decode(sys.stdout.encoding)
+    exit_code_string = str(out.returncode)
+    hash_string = hashlib.sha256(alias.encode("utf-8")).hexdigest()[:8]
+    filename = alias + "." + exit_code_string + "." + hash_string + ".md"
+    file_path = os.path.join(command_tests_path, filename)
+    f = open(file_path, "w")
+    f.write("# Command\n")
+    f.write("```json\n")
+    f.write(json.dumps(command))
+    f.write("\n")
+    f.write("```\n")
+    f.write("\n")
+    f.write("# Expected exit code\n")
+    f.write(exit_code_string)
+    f.write("\n\n")
+    f.write("# Expected output\n")
+    f.write("```\n")
+    f.write(output_string)
+    f.write("\n```\n")
+    f.close()
 
 def remove_all_command_tests_files(command_tests_directory):
     for file in os.listdir(command_tests_directory):
