8a55a4200c7bd07c29497c9ba4278cb3f66b7be4
Marcin Iwanicki <miwanicki1@bloomberg.net>
2020-07-07 08:56:37 +0100

    Bump version to 0.4.0 (#61)
    - Ran `make update_version` after updating the version in the `Makefile`
    
    Test Plan:
    
    - Verify CI checks pass
    
    Signed-off-by: Marcin Iwanicki <miwanicki1@bloomberg.net>

--- Sources/XCDiffCommand/Constants.swift
+++ Sources/XCDiffCommand/Constants.swift
@@ -24,7 +24,7 @@ final class Constants {
             debugVersionIdentifier(),
             gitHashVersionIdentifier(),
         ].compactMap { $0 }
-        return Version(0, 3, 0, buildMetadataIdentifiers: identifiers)
+        return Version(0, 4, 0, buildMetadataIdentifiers: identifiers)
     }()
 
     private static let gitHash = "#GIT_SHORT_HASH#"
